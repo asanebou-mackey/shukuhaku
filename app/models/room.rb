ca6cdf690098c72f00aa1d5d3819ac1c8fec2856
asanebou.mackey <asanebou.job4219@gmail.com>
2022-11-28 22:04:24 +0900

    バリデーションエラーメッセージの表示

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,5 +3,12 @@ class Room < ApplicationRecord
     has_many :reservations
 
     has_one_attached :image
-    
+
+    #バリデーションエラーメッセージ
+    validates :name, presence: true
+    validates :introduce, presence: true
+    validates :price, presence: true
+    validates :address, presence: true
+    validates :image, presence: true
+
 end
